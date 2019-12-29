45b09039f7454dc80549ae653daf5c417f8f2351
Dhroov Gupta <dhroovgupta7@gmail.com>
2019-12-29 16:19:47 +0530

    Updates

---
+++ precache-manifest.5525564e6614c62dc369c82866698a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038c9287d36d31045b9cad525fbadda0",
+    "url": "/CB-github/index.html"
+  },
+  {
+    "revision": "6b8c0db2af4dd3cc4a14",
+    "url": "/CB-github/static/css/main.c3d2686c.chunk.css"
+  },
+  {
+    "revision": "db19447afac1d71cfd06",
+    "url": "/CB-github/static/js/2.9e4e24fa.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/CB-github/static/js/2.9e4e24fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b8c0db2af4dd3cc4a14",
+    "url": "/CB-github/static/js/main.cfc6df6a.chunk.js"
+  },
+  {
+    "revision": "cba3ad526294ea280fce",
+    "url": "/CB-github/static/js/runtime-main.1bba5e7f.js"
+  }
+]);
